101434c95b09386cdde5e0a581238c2e7904d7a8
ZweHtetPaing45 <ko2097455@gmail.com>
2025-11-01 00:30:46 +0630

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -5,10 +5,15 @@ const bcryptjs=require('bcryptjs');
 const jwt=require('jsonwebtoken');
 
 const transporter = nodemailer.createTransport({
-    service : 'gmail',
+    host: 'smtp.gmail.com',
+    port: 456,
+    secure: true,
     auth : {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
